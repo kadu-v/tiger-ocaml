d5bf4c0be52cb9558688829526ffb188bf56de21
kadu-v <kazuonsunday@yahoo.co.jp>
2021-03-19 14:16:36 +0900

    Add: equal_token_list, token_list_test

---
+++ ktiger/test/util.ml
@@ -0,0 +1,15 @@
+open Core.Lexer
+open Core.Util
+open OUnit2
+
+let rec equal_token_list toks1 toks2 =
+  match (toks1, toks2) with
+  | [], [] | [ EOF ], [] | [], [ EOF ] -> true
+  | tok1 :: xs, tok2 :: ys ->
+      if equal_token tok1 tok2 then equal_token_list xs ys else false
+  | _ -> false
+
+let token_list_test name (expected : token list) (input : string) =
+  name >:: fun _ ->
+  assert_equal expected (string_tokenizer input) ~printer:show_token_list
+    ~cmp:equal_token_list
