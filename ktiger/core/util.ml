b505825cd3ac2c5fad16322ec8bd6eefdccce85d
kadu-v <kazuonsunday@yahoo.co.jp>
2021-03-19 14:15:59 +0900

    Add: show_token_list

--- ktiger/core/util.ml
+++ ktiger/core/util.ml
@@ -1,8 +1,15 @@
+open Lexer
 let rec string_tokenizer_aux buf =
   match Lexer.token buf with
-  | EOF -> [ Parser.EOF ]
+  | EOF -> [ EOF ]
   | tok -> tok :: string_tokenizer_aux buf
 
 let string_tokenizer str =
   let lexbuf = Lexing.from_string str in
   string_tokenizer_aux lexbuf
+
+let show_token_list (toks : token list) = String.concat ", " (List.map Lexer.show_token toks)
+
+
+
+
