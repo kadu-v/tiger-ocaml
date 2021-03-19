e82da4875c937b5a1913315c47890d7173a8401c
kadu-v <kazuonsunday@yahoo.co.jp>
2021-03-19 15:11:49 +0900

    Add: test_token

--- ktiger/test/util.ml
+++ ktiger/test/util.ml
@@ -2,6 +2,12 @@ open Core.Lexer
 open Core.Util
 open OUnit2
 
+let test_token name expected input =
+  let lexbuf = Lexing.from_string input in
+  let tok = token lexbuf in
+  name >:: fun _ ->
+  assert_equal expected tok ~printer:show_token ~cmp:equal_token
+
 let rec equal_token_list toks1 toks2 =
   match (toks1, toks2) with
   | [], [] | [ EOF ], [] | [], [ EOF ] -> true
