f26a011b23f10ec1a743c1cae46360c6c75917ef
kadu-v <kazuonsunday@yahoo.co.jp>
2021-04-11 02:33:44 +0900

    Add: actual_ty, equal_ty

--- ktiger/core/types.ml
+++ ktiger/core/types.ml
@@ -7,3 +7,13 @@ type ty = INT | STRING | UNIT | NIL
    | ARRAY of ty * unique
    | NAME of Symbol.symbol * ty option ref 
 *)
+
+let actual_ty t = t
+
+let equal_ty ty1 ty2 =
+  match (ty1, ty2) with
+  | INT, INT -> true
+  | STRING, STRING -> true
+  | UNIT, UNIT -> true
+  | NIL, NIL -> true
+  | _ -> false
